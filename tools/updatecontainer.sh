bbfd5c1c4f7562e5b85ca426eaf18283fd4ec295
Noeliel <noelieldev@gmail.com>
2021-01-18 16:16:37 +0100

    Fix hardcoded container image format

--- tools/updatecontainer.sh
+++ tools/updatecontainer.sh
@@ -6,8 +6,8 @@ echo "Updating $1..."
 oldid=$(docker images --filter=reference="$1" --format "{{.ID}}")
 echo "Old ID is $oldid"
 # the following is rather hacky, might require a proper redo
-docker stop "docker_$1_1"
-docker container rm "docker_$1_1"
+docker stop "docker-compose_$1_1"
+docker container rm "docker-compose_$1_1"
 docker rmi "$oldid"
 docker load -i "/srv/Backend/cd/$1/$1.tar"
 docker-compose up -d "$1"
