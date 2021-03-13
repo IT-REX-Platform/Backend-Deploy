221cf70131e30947dc4ece63fe848b48456c941a
GarondEisenfaust <chr.schr@web.de>
2021-03-13 20:30:30 +0100

    Quizservice is now deployed

--- tools/continuouslydeploy.sh
+++ tools/continuouslydeploy.sh
@@ -4,14 +4,16 @@ inotifywait -r -m /srv/Backend/cd/ -e create -e moved_to |
     while read dir action file; do
         # echo "The file '$file' appeared in directory '$dir' via '$action'"
         if [ "$file" = "deploy" ]; then
-        	# this could be done a little better but i'm lazy
+            # this could be done a little better but i'm lazy
             if [ "$dir" = "/srv/Backend/cd/gateway/" ]; then
                 image="gateway"
             elif [ "$dir" = "/srv/Backend/cd/courseservice/" ]; then
                 image="courseservice"
             elif [ "$dir" = "/srv/Backend/cd/mediaservice/" ]; then
                 image="mediaservice"
-            fi;
+            elif [ "$dir" = "/srv/Backend/cd/quizservice/" ]; then
+                image="quizservice"
+            fi
             sh updatecontainer.sh "$image"
-        fi;
+        fi
     done
